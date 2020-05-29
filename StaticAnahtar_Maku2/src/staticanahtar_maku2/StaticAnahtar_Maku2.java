23810abe60734814547a72fff54ec8aad9350ba0
cankartal48 <66039760+cankartal48@users.noreply.github.com>
2020-05-29 16:15:09 +0300

    Add files via upload

---
+++ StaticAnahtar_Maku2/src/staticanahtar_maku2/StaticAnahtar_Maku2.java
@@ -0,0 +1,15 @@
+
+package staticanahtar_maku2;
+
+import java.util.Scanner;
+
+public class StaticAnahtar_Maku2 {
+static Scanner s=new Scanner(System.in);
+    public static void main(String[] args) {
+    
+     Statik.sayi1=Integer.parseInt(s.next());
+    Statik.sayi2=Integer.parseInt(s.next());
+    System.out.print(Statik.topla());
+    }
+    
+}
---
+++ StaticAnahtar_Maku2/src/staticanahtar_maku2/Statik.java
@@ -0,0 +1,11 @@
+
+package staticanahtar_maku2;
+
+public class Statik {
+    public static int sayi1;
+    public static int sayi2;
+ 
+    public static int topla(){
+ 
+        return sayi1+sayi2;
+}}
